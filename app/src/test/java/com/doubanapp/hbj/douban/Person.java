ecc53352f498b669bbc10bbf6524acbab077e93f
1085069832 <1085069832@qq.com>
2017-05-11 19:16:38 +0800

    Test

--- app/src/test/java/com/doubanapp/hbj/douban/Person.java
+++ app/src/test/java/com/doubanapp/hbj/douban/Person.java
@@ -3,8 +3,13 @@ package com.doubanapp.hbj.douban;
 /**
  * Created by Administrator on 2017/5/3 0003.
  */
-public class Person {
+public class Person extends Person2 {
     public String name;
     public String sex;
     public int age;
+
+    public Person(String res, int n, String sex) {
+        super(res, n);
+        this.sex = sex;
+    }
 }
---
+++ app/src/test/java/com/doubanapp/hbj/douban/Person2.java
@@ -0,0 +1,14 @@
+package com.doubanapp.hbj.douban;
+
+/**
+ * Created by Administrator on 2017/5/11 0011.
+ */
+public class Person2 {
+
+    private String res;
+    private int n;
+    public Person2(String res,int n) {
+        this.res = res;
+        this.n = n;
+    }
+}
